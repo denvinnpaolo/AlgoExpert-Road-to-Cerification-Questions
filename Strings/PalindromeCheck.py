91ea3099a988d1b0d8f65718998ca234537a5981
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-11 14:40:35 -0800

    added palindrome check structure

---
+++ Strings/PalindromeCheck.py
@@ -0,0 +1,11 @@
+# Palindrome Check
+# Difficulty: Easy
+
+# Instruction:
+
+#   Write a function that takes in a non-empty string and that returns a boolean
+#   representing whether the string is a palindrome.
+
+#   A palindrome is defined as a string that's written the same forward and
+#   backward. Note that single-character strings are palindromes.
+
