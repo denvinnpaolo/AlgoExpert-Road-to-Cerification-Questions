a87c5ea9ab6149cef937b41b184667493d35353c
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-11 14:43:17 -0800

    finished palindrome question

--- Strings/PalindromeCheck.py
+++ Strings/PalindromeCheck.py
@@ -9,3 +9,15 @@
 #   A palindrome is defined as a string that's written the same forward and
 #   backward. Note that single-character strings are palindromes.
 
+# Solution
+
+def isPalindrome(string):
+    # Write your code here.
+	ans = True
+    for i in range(len(string)):
+		if string[i] == string[len(string) - i - 1]:
+			continue
+		else:
+			ans = False
+			
+	return ans
