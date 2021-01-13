7634a1c27297da0b540c6dca0aeed68fcdb8417e
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-13 15:57:09 -0800

    finished validate BST

--- Binary_Trees/ValidateBst.py
+++ Binary_Trees/ValidateBst.py
@@ -2,3 +2,27 @@
 # Difficulty: Medium
 
 # Instruction:
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def validateBst(tree):
+    # Write your code here.
+    return validateBstHelper(tree, float('-inf'), float('inf'))
+
+def validateBstHelper(tree, min_value, max_value):
+	if tree is None:
+		return True
+	
+	if tree.value < min_value or tree.value >= max_value:
+		return False
+	
+	left_is_val = validateBstHelper(tree.left, min_value, tree.value)
+	
+	return left_is_val and validateBstHelper(tree.right, tree.value, max_value)
+	
+	
+		
