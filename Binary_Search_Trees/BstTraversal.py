9e6d817ba3fc7b8a760bcbb32dcf95f564eb43bc
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-13 19:05:41 -0800

    preorder traversal finished

--- Binary_Search_Trees/BstTraversal.py
+++ Binary_Search_Trees/BstTraversal.py
@@ -11,3 +11,12 @@ def inOrderTraverse(tree, array):
 		inOrderTraverse(tree.right, array)
 
 	return array
+
+def preOrderTraverse(tree, array):
+    # Write your code here.
+    if tree is not None:
+		array.append(tree.value)
+		preOrderTraverse(tree.left, array)
+		preOrderTraverse(tree.right, array)
+
+	return array
