c4460bc3cff1e4dfdcc872cefe59373f36267f0b
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-14 20:58:47 -0800

    invert binary tree solution 1 finished

--- Binary_Trees/InvertBinaryTrees.py
+++ Binary_Trees/InvertBinaryTrees.py
@@ -2,3 +2,25 @@
 # Difficulty: Medium
 
 # Solution1:
+def invertBinaryTree(tree):
+    # Write your code here.
+	queue = [tree]
+	
+	while queue:
+		cur = queue.pop()
+		
+		if cur is None:
+			continue
+		cur.left, cur.right = cur.right, cur.left
+		
+		queue.append(cur.left)
+		queue.append(cur.right)
+		
+		
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
