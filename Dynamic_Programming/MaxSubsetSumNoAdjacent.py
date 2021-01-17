864b482edf333d7c7df365bff414b99c28815438
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-16 18:12:59 -0800

    finished Max Subset Sum No Adjacent Problem

--- Dynamic_Programming/MaxSubsetSumNoAdjacent.py
+++ Dynamic_Programming/MaxSubsetSumNoAdjacent.py
@@ -10,3 +10,20 @@
 
 # Solution:
 
+def maxSubsetSumNoAdjacent(array):
+    # Write your code here.
+	if len(array) == 0: 
+		return 0
+	elif len(array) == 1:
+		return array[0]
+	else:
+		maxSums = [0] * (len(array))
+		for i in range(len(array)):
+			maxSums[i] = max(maxSums[i - 1], maxSums[i - 2] + array[i])
+			print(maxSums[i - 2],  array[i])
+			print(maxSums)
+		
+			
+		return max(maxSums[len(array) - 1], maxSums[len(array)-2])
+
+				
