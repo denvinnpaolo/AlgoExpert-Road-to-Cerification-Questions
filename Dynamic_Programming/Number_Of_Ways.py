00b5a1f121c260141e358f90e9fa911b633d2e79
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-16 22:38:42 -0800

    instruction added

--- Dynamic_Programming/Number_Of_Ways.py
+++ Dynamic_Programming/Number_Of_Ways.py
@@ -4,7 +4,9 @@
 # Instruction:
 
 #   Given an array of distinct positive integers representing coin denominations and a
-#   single non-negative integer 
+#   single non-negative integer n  representing a target amount of
+#   money, write a function that returns the number of ways to make change for
+#   that target amount using the given coin denominations.
 
 # Solution:
 def numberOfWaysToMakeChange(n, denoms):
