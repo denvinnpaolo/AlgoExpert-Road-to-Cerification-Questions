eeba29fd8c23c3fcfb6360d2f9b549e7bdb53e8d
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-17 20:39:45 -0800

    finished move element to end problem

--- Arrays/MoveElementToEnd.py
+++ Arrays/MoveElementToEnd.py
@@ -2,3 +2,20 @@
 # Difficulty: Medium
 
 # Solution:
+def moveElementToEnd(array, toMove):
+    # Write your code here.
+	for i in range(len(array)):
+		if array[i] == toMove:
+			array.pop(i)
+			array.append(toMove)
+
+    for i in range(len(array)):
+		if array[i] == toMove:
+			array.pop(i)
+			array.append(toMove)
+    for i in range(len(array)):
+		if array[i] == toMove:
+			array.pop(i)
+			array.append(toMove)
+
+    return array
