5d730b72a7647c4e8f07b611d65459441dd7fd2c
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-17 18:32:48 -0800

    solving number of ways to make change

--- Dynamic_Programming/Number_Of_Ways.py
+++ Dynamic_Programming/Number_Of_Ways.py
@@ -11,27 +11,59 @@
 # Solution:
 def numberOfWaysToMakeChange(n, denoms):
     # Write your code here.
-    if n == 0:
+	if n == 0:
 		return 1
-	
 	if len(denoms) == 1:
 		if n % denoms[0] == 0:
 			return 1
-		else: return 0
-	
-	
-	
-	ans = 0
-	for i in range(len(denoms)):
-		if n % denoms[i] == 0:
-			ans += 1
-			print(ans)
 		else:
-			numNeeded = n % denoms[i]
-			print(numNeeded)
-			if numNeeded in denoms:
-				ans += 1
-			elif denoms[0] == 1:
-				ans += 1
+			return 0
+	ans = 0
+	d= {}
+	a= []
+	multiplier = 1
+	
+	for i in range(len(denoms)):
+		while denoms[i] * multiplier <= n:
+			num = denoms[i] * multiplier
+			d[num] = num
+			if num not in a:
+				a.append(num)
+				
+			multiplier += 1
+		multiplier = 1
 
-	return ans
+	for i in range(len(denoms)):
+		while denoms[i] * multiplier <= n:
+			num = n - denoms[i] * multiplier 
+			multiplier += 1
+			if num in d:
+				ans += 1
+				
+		multiplier = 1
+
+
+    # if n == 0:
+	# 	return 1
+	
+	# if len(denoms) == 1:
+	# 	if n % denoms[0] == 0:
+	# 		return 1
+	# 	else: return 0
+	
+	
+	
+	# ans = 0
+	# for i in range(len(denoms)):
+	# 	if n % denoms[i] == 0:
+	# 		ans += 1
+	# 		print(ans)
+	# 	else:
+	# 		numNeeded = n % denoms[i]
+	# 		print(numNeeded)
+	# 		if numNeeded in denoms:
+	# 			ans += 1
+	# 		elif denoms[0] == 1:
+	# 			ans += 1
+
+	# return ans
