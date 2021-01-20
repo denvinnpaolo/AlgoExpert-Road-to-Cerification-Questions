7b6494c8e27b2098c9f3522651c7442c6af1e033
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-20 11:40:55 -0800

    added instructions

---
+++ Dynamic_Programming/Min_Num_Coins.py
@@ -0,0 +1,10 @@
+# Minimum number of coins to make change
+# Difficulty: Medium
+
+# Instruction:
+
+#   Given an array of positive integers representing coin denominations and a
+#   single non-negative integer n  representing a target amount of
+#   money, write a function that returns the smallest number of coins needed to
+#   make change for (to sum up to) that target amount using the given coin
+#   denominations.
