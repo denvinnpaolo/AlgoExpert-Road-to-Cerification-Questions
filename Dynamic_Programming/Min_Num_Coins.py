6d19517cb645950066350cfbeac37041c7e88b6a
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-20 15:51:06 -0800

    finished

--- Dynamic_Programming/Min_Num_Coins.py
+++ Dynamic_Programming/Min_Num_Coins.py
@@ -8,3 +8,13 @@
 #   money, write a function that returns the smallest number of coins needed to
 #   make change for (to sum up to) that target amount using the given coin
 #   denominations.
+
+# Solution:
+def minNumberOfCoinsForChange(n, denoms):
+	minCoins = [float('inf') for amount in range(n + 1)]
+	minCoins[0] = 0
+	for denom in denoms:
+		for amount in range(len(minCoins)):
+			if denom <= amount:
+				minCoins[amount] = min(minCoins[amount], minCoins[amount - denom] + 1)
+	return minCoins[n] if minCoins[n] != float('inf') else  -1
