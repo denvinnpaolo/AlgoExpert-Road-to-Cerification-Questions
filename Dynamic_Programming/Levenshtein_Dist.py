d9d088c524b33be366894c22e5906b931d359c1f
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-21 10:05:44 -0800

    added instruction

--- Dynamic_Programming/Levenshtein_Dist.py
+++ Dynamic_Programming/Levenshtein_Dist.py
@@ -1,7 +1,14 @@
 # Levenshtein Distance
 # Difficulty: Medium
 
-# 
+# Instructions:
+
+#   Write a function that takes in two strings and returns the minimum number of
+#   edit operations that need to be performed on the first string to obtain the
+#   second string.
+
+#   There are three edit operations: insertion of a character, deletion of a
+#   character, and substitution of a character for another.
 
 # Solution
 def levenshteinDistance(str1, str2):
