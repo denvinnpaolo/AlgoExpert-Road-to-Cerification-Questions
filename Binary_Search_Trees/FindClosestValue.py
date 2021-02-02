8acb14dad8c0b4e3abb1061e5201e972c32d00d0
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-02-01 16:26:19 -0800

    added iteration solution for find closest value in bst

--- Binary_Search_Trees/FindClosestValue.py
+++ Binary_Search_Trees/FindClosestValue.py
@@ -23,6 +23,29 @@ def closestValueHelper(tree, target, closest):
 	
 # Solution (Iteration):
 
+def findClosestValueInBst(tree, target):
+    # Write your code here.
+	# cur is the current node to be checked 	
+	cur = tree
+	# closest initial value will be the very first value 	
+	closest = cur.value
+	
+	# this while loop is going to execute until it hits a "None" 	
+	while cur is not None:
+		#if statement to check if the current node value minus target is
+		#less than current closest number to the target		
+		if abs(cur.value - target) < abs(closest - target):
+			closest = cur.value
+	 		
+		if target < cur.value:
+			cur = cur.left
+		elif target > cur.value:
+			cur = cur.right
+		else:
+			break
+
+	return closest
+
 # This is the class of the input tree. Do not edit.
 class BST:
     def __init__(self, value):
