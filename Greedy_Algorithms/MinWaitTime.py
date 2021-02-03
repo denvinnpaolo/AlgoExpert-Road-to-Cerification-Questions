1157cf7ca92d84b5c548af3a1bdc2a81b0c9cc13
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-02-03 15:47:48 -0800

    added MinWaitTime solution 1

--- Greedy_Algorithms/MinWaitTime.py
+++ Greedy_Algorithms/MinWaitTime.py
@@ -2,4 +2,18 @@
 # Easy
 # Greedy Algorithm
 
-# Solution
+# Solution 1:
+
+def MinumumWaitingTime(queries):
+    # sorts the query for optimal order for min waiting time
+    q = queries.sorted()
+    # min wait time variable
+    time = 0
+
+    # take the index and val of sorted query
+    for i, val in enumerate(q):
+        queriesLeft = len(q) - (i - 1)
+        time += val * queriesLeft
+
+
+    return time
