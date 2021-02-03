48cf9c433f4d85b154bc1342c9b86f1b1314e847
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-02-03 15:51:52 -0800

    added MinWaitTime solution 2

--- Greedy_Algorithms/MinWaitTime.py
+++ Greedy_Algorithms/MinWaitTime.py
@@ -17,3 +17,20 @@ def MinumumWaitingTime(queries):
 
 
     return time
+
+
+# Solution 2:
+def MinimumWaitingTime(queries):
+    queries.sort()
+    a = [0] * len(queries)
+    time = 0
+
+    for i in range(len(queries) - 1):
+        # since we know how much duration the next value is going to be
+        # we set the array at next index by
+        # adding the value of the query at the same index as the value in the a array
+        a[i + 1] = queries[i] + a[i]
+        #adding the duration as function move along the query 
+        time += a[i+1]
+
+    return time
