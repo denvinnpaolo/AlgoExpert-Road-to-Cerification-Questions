a8da8e6fadf84875fdb75d9e665f78393214c3d7
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-02-03 20:32:40 -0800

    added comments to RemoveDuplicates

--- Linked_List/RemoveDuplicate.py
+++ Linked_List/RemoveDuplicate.py
@@ -6,10 +6,13 @@
 
 def RemoveDuplicateFromLinkedList(linkedList):
     cur = linkedList
-
+    # while cur is not None keep iterating
     while cur:
+        # while there is a next node and current node value is equal to the value of the next node keep iterating
         while cur.next and cur.value == cur.next.value:
+            # changes next value to the next of the next node
             cur.next = cur.next.next
+        # makes the next node the current node
         cur = cur.next
 
     return linkedList
