2968acd34caa529e354f5cf725d669f2385ce368
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-02-16 14:59:07 -0800

    finished non-constructable change

--- Arrays/NonConstructable.py
+++ Arrays/NonConstructable.py
@@ -3,3 +3,24 @@
 # Array
 
 # Solution:
+def nonConstructibleChange(coins):
+    # Write your code here.
+	
+#Understand:
+# given an array of positive integers representing values of coins
+# goal: write a function that returns the minimum sum of money that you cannot 
+# 	create with the coins in your possession 
+
+# Plan:
+# Sort the array to have a better perspective of the lowest number to highest
+# for loop to go through the coins
+	
+	coins.sort()
+	change = 0
+	
+	for coin in coins:
+		if change + 1 < coin:
+			return change + 1
+		change += coin
+			
+	return change+1
